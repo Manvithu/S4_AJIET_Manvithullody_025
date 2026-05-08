54a418452f93fc8c9246878dca9430f86f759151
Manvithu <manvithax5@gmail.com>
2026-05-08 14:43:36 +0530

    Add Array1 class to print array elements
    java code to read & display the arrays

---
+++ array.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String[] args) {
+         int[] numbers = {1, 2, 3, 4, 5};
+         System.out.println("elements are:");
+         for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
