f65a7b0680432a951797d6d815b273f4e413b87b
Manvithu <manvithax5@gmail.com>
2026-05-09 14:30:23 +0530

    Add Swap class to demonstrate variable swapping
    java code to swap 2 numbers

---
+++ Swap.java
@@ -0,0 +1,16 @@
+class Swap {
+     public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int temp;
+        System.out.println("Before Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
